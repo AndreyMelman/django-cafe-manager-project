713b9bd7d8e366cd2ea5d5b7a5ee385c3b2e2928
Andrei Lahunovich <lagunmelman@gmail.com>
2025-02-21 17:47:00 +0300

    new urls for  views

---
+++ cafe_manager/orders/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import (
+    OrderListView,
+    AddOrderCreateView,
+    UpdateStatusView,
+    DeleteOrderView,
+    RevenueView,
+)
+
+urlpatterns = [
+    path("", OrderListView.as_view(), name="order_list"),
+    path("add/", AddOrderCreateView.as_view(), name="add_order"),
+    path("update_status/<int:pk>/", UpdateStatusView.as_view(), name="update_status"),
+    path("delete_order/<int:pk>/", DeleteOrderView.as_view(), name="delete_order"),
+    path("revenue/", RevenueView.as_view(), name="revenue"),
+]
