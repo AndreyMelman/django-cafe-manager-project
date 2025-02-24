7519e4909aea7c30d55687ef5b28db94c3b6456b
Andrei Lahunovich <lagunmelman@gmail.com>
2025-02-25 00:57:57 +0300

    change urls for api

--- cafe_manager/cafe_manager/urls.py
+++ cafe_manager/cafe_manager/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("orders/", include("orders.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('orders.urls')),
 ]
